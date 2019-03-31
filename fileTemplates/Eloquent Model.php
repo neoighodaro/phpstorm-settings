d5bb0b8f4f03d0d4849a737adfc34b9f9b17df53
Neo Ighodaro <neo@creativitykills.co>
2019-03-31 09:28:26 +0200

    PS-2019.1 <neo@Neos-MacBook-Pro.local Create markdown.xml, PHP Class.php, _@user_All hallow_s eve.icls, project.default.xml, diff.xml, Default copy.xml, Default.xml, _@user_Material Deep Ocean.icls, _@user_Monokai.icls, find.xml, _@user_Cobalt.icls, code.style.schemes.xml, _@user_Material Oceanic.icls, _@user_WarmNeon.icls, _@user_Darcula.icls, _@user_Blackboard.icls, JavaScript File.js, _@user_Twilight.icls, _@user_Material Solarized Dark.icls, Default.xml, PHP.xml, _@user_Material Darker.icls, _@user_Monokai Pro.icls, _@user_VibrantInk.icls, _@user_Arc Dark.icls, _@user_Night Owl _No Italic_.icls, _@user_Night Owl Palenight _No Italic_.icls, _@user_Railscasts.icls, editor.codeinsight.xml, keymap.xml, night-owl-theme.xml, editor.xml, debugger.xml, Laravel.xml, PHP File.php, updates.xml, baseRefactoring.xml, ide.general.xml, _@user_High ontrast.icls, rainbow_brackets.xml, git.xml, _@user_Night Owl Palenight.icls, _@user_Night Owl Carbon.icls, ui.lnf.xml, filetypes.xml, php.xml, plugin_ui.xml, colors.scheme.xml, Eloquent Model.php, PHP Trait.php, _@user_Material Palenight.icls, material_custom_theme.xml, MrZuraOneDark_0.icls, intentionSettings.xml, laf.xml, _@user_Atom One Dark.icls, macros.xml, material_theme.xml, _@user_Night Owl Carbon _No Italic_.icls, vcs.xml, PHP Interface.php, _@user_Night Owl.icls

---
+++ fileTemplates/Eloquent Model.php
@@ -0,0 +1,17 @@
+<?php
+
+#if (${NAMESPACE})
+namespace ${NAMESPACE};
+#end
+
+use Illuminate\Database\Eloquent\Model;
+
+class ${NAME} extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected \$fillable = [];
+}
